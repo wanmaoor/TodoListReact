092b8429872561cf80e59b45f9611f472bee9214
wanma <ww>
2019-06-13 22:35:38 +0800

    Updates

---
+++ precache-manifest.763abe2fad2834f97eb66d169ff8f6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5f916676442ad2881e2b",
+    "url": "/static/js/main.5f916676.chunk.js"
+  },
+  {
+    "revision": "1bbd4b8723cccb12031f",
+    "url": "/static/js/2.1bbd4b87.chunk.js"
+  },
+  {
+    "revision": "5f916676442ad2881e2b",
+    "url": "/static/css/main.e55f5927.chunk.css"
+  },
+  {
+    "revision": "dd26c361069438f6cb32f385d7651030",
+    "url": "/index.html"
+  }
+];
