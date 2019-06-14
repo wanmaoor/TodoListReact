7c9ce2f80a11f122ce2371d21a3b7decae27a65e
wanma <ww>
2019-06-14 22:39:13 +0800

    change upload dir

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class AddTodo extends Component {
+  state = {
+    title: ''
+  }
+  onChange = (e) => {
+    this.setState({title: e.target.value})
+  }
+  
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.addTodo(this.state.title)
+    this.setState({title: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <form 
+          action="" 
+          style={{display: 'flex'}}
+          onSubmit={this.onSubmit}
+        >
+          <input 
+            type="text" 
+            name="title" 
+            placeholder="Add Todo ..." 
+            style={{flex: '10', padding: '5px'}}
+            value={this.state.title}
+            onChange={this.onChange} 
+          />
+          <input 
+            type="submit" 
+            value="Submit" 
+            className="btn" 
+            style={{flex: '1'}} 
+          />
+        </form>
+      </div>
+    );
+  }
+}
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+};
+export default AddTodo;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>this is the todolist app v1.0.0. it is part of a react crash course</p>
+    </React.Fragment>
+  )
+}
