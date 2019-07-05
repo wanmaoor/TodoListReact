6cfa5958ddee12cdd8cdc0166cd0bcd4e6d5878c
wanmaoor <wanmao1284879375@outlook.com>
2019-07-05 10:24:33 +0800

    build

---
+++ build/precache-manifest.5048ad9c5fdec16c1b9ac6bd3eab5054.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16b0260cdabc778bff97",
+    "url": "/TodoListReact/static/js/runtime~main.16b0260c.js"
+  },
+  {
+    "revision": "385645fe15adfe00a558",
+    "url": "/TodoListReact/static/js/main.385645fe.chunk.js"
+  },
+  {
+    "revision": "6bbe338c094506c57e1d",
+    "url": "/TodoListReact/static/js/2.6bbe338c.chunk.js"
+  },
+  {
+    "revision": "385645fe15adfe00a558",
+    "url": "/TodoListReact/static/css/main.90a1017b.chunk.css"
+  },
+  {
+    "revision": "03950a06793f937becb5913f17be1f86",
+    "url": "/TodoListReact/index.html"
+  }
+];
